1f009a24e1777b6b48b9296efeee7c59080ac08f
SubhiramGurlinka <150935371+SubhiramGurlinka@users.noreply.github.com>
2024-02-27 12:16:43 +0530

    Update mysql8.2_rocky8_script.sh

--- mysql8.2_rocky8_script.sh
+++ mysql8.2_rocky8_script.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Install necessary packages
 yum install -y wget sudo perl libaio numactl net-tools
-
-curl -O https://raw.githubusercontent.com/jgstew/tools/master/bash/install_bigfix.sh
-
-bash install_bigfix.sh 10.115.170.160
 
 # Download MySQL RPM bundle
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-8.2.0-1.el8.x86_64.rpm-bundle.tar
