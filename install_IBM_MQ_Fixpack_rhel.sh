70fa318633fbfeb8d1d1c62340cd577633060fe8
SubhiramGurlinka <150935371+SubhiramGurlinka@users.noreply.github.com>
2024-03-07 11:47:34 +0530

    Update install_IBM_MQ_Fixpack_rhel.sh

--- install_IBM_MQ_Fixpack_rhel.sh
+++ install_IBM_MQ_Fixpack_rhel.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# this script will install the fixpack for IBM MQ by taking the file name as input
+# Usage (single line):
+#   wget "https://raw.githubusercontent.com/SubhiramGurlinka/bigfix_scripts/main/install_IBM_MQ_Fixpack_rhel.sh"; bash install_IBM_MQ_Fixpack_rhel.sh "9.2.0-IBM-MQ-LinuxX64-FP0020.tar.gz"
+# OR
+# Usage:
+#   wget "https://raw.githubusercontent.com/SubhiramGurlinka/bigfix_scripts/main/install_IBM_MQ_Fixpack_rhel.sh"
+#   bash install_IBM_MQ_Fixpack_rhel.sh "9.2.0-IBM-MQ-LinuxX64-FP0020.tar.gz"
 
 # Check if a file name is provided as an argument
 if [ $# -eq 0 ]; then
